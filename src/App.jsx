c2116620c1afcc64ef45c15db140b20b683a6192
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-18 16:44:16 +0400

    new commit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Header from "./components/Header/Header";
 function App() {
-  return <div className=" min-h-screen"></div>;
+  return (
+    <div className="bg-white min-h-screen flex justify-center	items-center">
+      <main className="h-900  ">
+        <Header></Header>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+  return (
+    <section>
+        <h2>Notifications</h2>
+        <div>3</div>
+        <h3>Mark all as read</h3>
+    </section>
+  )
+}
+
+export default Header
