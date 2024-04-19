5496008716cb2bfaf0b57c657cc97ec0ab81b568
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-19 14:52:26 +0400

    new commit!

--- src/components/unmarkedComments/Unmarked.jsx
+++ src/components/unmarkedComments/Unmarked.jsx
@@ -4,15 +4,15 @@ import img6 from "/images/image-chess.webp";
 export default function Unmarked() {
   return (
     <section className="">
-      <div className="gap-[10px]  flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img4} className="w-[45px] h-[45px]"></img>
+      <div className="gap-[7px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+        <img src={img4} className="mr-4  w-[45px] h-[45px]"></img>
         <span className="text-very-dark-grey-blue text-[15px] font-bold  custom-font-style">
           Rizky Hasanuddin
         </span>{" "}
         sent you a private message
-        <span className="font-medium opacity-60	grey-blue"> 5 days ago </span>
+        <span className=" absolute top-12 left-[13%] font-medium opacity-60	grey-blue"> 5 days ago </span>
       </div>
-      <div className="ml-[5rem] font-medium  px-[20px] py-[20px] pt-[12px] text-[16px] w-[566px] h-[97px] border border-solid border-very-light-grey-blue bg-white rounded-[5px]">
+      <div className="ml-[5.5rem] font-medium  px-[20px] py-[20px] pt-[12px] text-[16px] w-[566px] h-[97px] border border-solid border-very-light-grey-blue bg-white rounded-[5px]">
         Hello, thanks for setting up the Chess Club. I’ve been a member for a
         few weeks now and I’m already having lots of fun and improving my game.
       </div>
