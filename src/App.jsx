cc65c378ce1009a11a10477fd22cf1b636882fb3
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-19 12:21:05 +0400

    new commit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 // import "./fix.css"
 import Header from "./components/Header/Header";
+import Marked from "./components/Header/markedComments/Marked";
 function App() {
   return (
     <main className="w-[730px] bg-white h-[839px] pt-[32px] pb-[17px] pl-[30px] pr-[30px]">
       <Header></Header>
+      <Marked></Marked>
     </main>
   );
 }
