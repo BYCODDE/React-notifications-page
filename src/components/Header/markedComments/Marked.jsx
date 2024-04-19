d9ea609e1d2fbdcc41ff945ae426e2f5663804c6
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-19 12:59:22 +0400

    new commit!

--- src/components/Header/markedComments/Marked.jsx
+++ src/components/Header/markedComments/Marked.jsx
@@ -2,14 +2,14 @@ import img1 from "/images/avatar-mark-webber.webp";
 export default function Marked() {
   return (
     <section className="">
-      <div className="rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+      <div className=" flex justify-between items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
         <img src={img1} alt="first_img" className="w-[45px] h-[45px]" />
         <span className="text-very-dark-grey-blue text-[14px] font-bold  custom-font-style">
           Mark Webber
         </span>{" "}
         reacted to your recent post{" "}
         <span className="dark-grey-blue text-[14px] font-bold  custom-font-style">
-          My first tournament today! 
+          My first tournament today!
         </span>
         <div className="w-[8px] h-[8px] bg-red-2 rounded "></div>
         1m ago
