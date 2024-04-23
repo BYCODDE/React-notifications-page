52422f3f0903dd02dc0b739d58b5528329e42791
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-23 11:21:16 +0400

    working on responsive!

--- src/components/markedComments/Marked.jsx
+++ src/components/markedComments/Marked.jsx
@@ -4,14 +4,18 @@ import img3 from "/images/avatar-jacob-thompson.webp";
 
 export default function Marked() {
   return (
-    <section className="mt-[31px]">   //TODO:"paddingebi gavasworo!ese: pt-0 pb-0 pl-0 "
-      <div className="{max-w-screen-md  p-4 pr-20  pt-0 pb-0 pl-0 }    text-[16px] gap-[7px] relative flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img1} alt="first_img" className=" {max-w-screen-md  w-[35px] h-[35px] }   mr-4 w-[45px] h-[45px]" />
-        <span className="max-w-screen-md  text-[14px]    text-very-dark-grey-blue  font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
+    <section className="mt-[31px]">
+      <div className="    text-[16px] gap-[7px] relative flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+        <img
+          src={img1}
+          alt="first_img"
+          className="    mr-4 w-[45px] h-[45px]"
+        />
+        <span className="   text-very-dark-grey-blue  font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
           Mark Webber
         </span>{" "}
         reacted to your recent post{" "}
-        <span className=" max-w-screen-md  text-[14px]    dark-grey-blue  font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
+        <span className="    dark-grey-blue  font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
           My first tournament today!
         </span>
         <div className="   w-[8px] h-[8px] bg-red-2 rounded "></div>
@@ -19,9 +23,9 @@ export default function Marked() {
           1m ago{" "}
         </span>
       </div>
-      <div className=" {max-w-screen-md  p-4 pr-20  }  relative my-[8px] gap-[7px]  flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img2} className="{max-w-screen-md  w-[35px] h-[35px] }    mr-4  w-[45px] h-[45px]"></img>
-        <span className="{max-w-screen-md  text-[14px] }   text-very-dark-grey-blue text-[15px] font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
+      <div className="   relative my-[8px] gap-[7px]  flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+        <img src={img2} className="  mr-4  w-[45px] h-[45px]"></img>
+        <span className="  text-very-dark-grey-blue text-[15px] font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
           Angela Gray
         </span>{" "}
         followed you <div className="w-[8px] h-[8px] bg-red-2 rounded "></div>
@@ -29,9 +33,9 @@ export default function Marked() {
           5m ago{" "}
         </span>
       </div>
-      <div className="{max-w-screen-md  p-4 pr-20  }     relative   gap-[7px]  flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img3} className="{max-w-screen-md  w-[35px] h-[35px] }     mr-4  w-[45px] h-[45px]"></img>
-        <span className="{max-w-screen-md  text-[14px] }   text-very-dark-grey-blue text-[15px] font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
+      <div className="   relative   gap-[7px]  flex justify-start items-center rounded-[8px] bg-snow pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+        <img src={img3} className="    mr-4  w-[45px] h-[45px]"></img>
+        <span className="  text-very-dark-grey-blue text-[15px] font-extrabold	  custom-font-style hover:text-blue-1 cursor-pointer">
           Jacob Thompson
         </span>{" "}
         has joined your group
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "768px" },
+
+      sm: { max: "639px" },
+    },
+
     extend: {
       fontFamily: {
         sans: ["CustomFont", "sans-serif"],
