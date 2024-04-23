757ad010e843627c34a38068889a7e7b678edd5e
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-23 14:37:20 +0400

    new commit!

--- src/components/unmarkedComments/Unmarked.jsx
+++ src/components/unmarkedComments/Unmarked.jsx
@@ -6,24 +6,31 @@ import img8 from "/images/avatar-anna-kim.webp";
 export default function Unmarked() {
   return (
     <section className="">
-      <div className=" gap-[7px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img4} className="md:w-[39px] md:h-[39px]  mr-4  w-[45px] h-[45px]  "></img>
+      <div className="md:pt-[5px] md:pb-[16px] md:pl-[16px] md:pr-[22px] md:gap-[3px] md:whitespace-nowrap gap-[7px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
+        <img
+          src={img4}
+          className="md:mr-1 md:w-[39px] md:h-[39px]  mr-4  w-[45px] h-[45px]  "
+        ></img>
         <span className="   md:text-[14px]  text-very-dark-grey-blue text-[15px] font-extrabold	 custom-font-style hover:text-blue-1 cursor-pointer">
           Rizky Hasanuddin
         </span>{" "}
-        sent you a private message
-        <span className=" absolute top-12 left-[13%] font-medium opacity-60	grey-blue">
+        sent you a private{" "}
+        <span className="md:top-[2rem] md:left-[18%] md:absolute">message</span>
+        <span className=" absolute top-12 left-[18%] font-medium opacity-60	grey-blue">
           {" "}
           5 days ago{" "}
         </span>
       </div>
-      <div className=" md:p-[16px]  md:w-[263px] md:h-[auto]   md:text-[14px]  hover:bg-light-grey-blue cursor-pointer   ml-[5.5rem] mb-[15px] font-medium  pt-2 pb-2 pl-3 pr-3 text-[16px] w-[544px] h-[97px] border border-solid border-very-light-grey-blue bg-white rounded-[5px]">
+      <div className="md:mt-[12px]  md:ml-[4rem] md:p-[16px]  md:w-[263px] md:h-[auto]   md:text-[14px]  hover:bg-light-grey-blue cursor-pointer   ml-[5.5rem] mb-[15px] font-medium  pt-2 pb-2 pl-3 pr-3 text-[16px] w-[544px] h-[97px] border border-solid border-very-light-grey-blue bg-white rounded-[5px]">
         Hello, thanks for setting up the Chess Club. I’ve been a member for a
         few weeks now and I’m already having lots of fun and improving my game.
       </div>
 
       <div className="    gap-[7px] mb-[26px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img5} className="md:w-[39px] md:h-[39px]    mr-4 w-[45px] h-[45px]  "></img>
+        <img
+          src={img5}
+          className="md:w-[39px] md:h-[39px]    mr-4 w-[45px] h-[45px]  "
+        ></img>
         <span className=" md:text-[14px]   text-very-dark-grey-blue text-[15px] font-extrabold	 custom-font-style hover:text-blue-1 cursor-pointer">
           Kimberly Smith
         </span>{" "}
@@ -39,7 +46,10 @@ export default function Unmarked() {
       </div>
 
       <div className=" {    mb-[26px] gap-[7px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img7} className="md:w-[39px] md:h-[39px]   mr-4 w-[45px] h-[45px]  "></img>
+        <img
+          src={img7}
+          className="md:w-[39px] md:h-[39px]   mr-4 w-[45px] h-[45px]  "
+        ></img>
         <span className="  md:text-[14px]   text-very-dark-grey-blue text-[15px] font-extrabold	 custom-font-style  hover:text-blue-1 cursor-pointer">
           Nathan Peterson
         </span>{" "}
@@ -58,7 +68,10 @@ export default function Unmarked() {
         </div>
       </div>
       <div className="   gap-[7px] relative flex justify-start items-center rounded-[8px] bg-white pt-[18px] pb-[17px] pl-[20px] pr-[32px]">
-        <img src={img8} className="md:w-[39px] md:h-[39px]   mr-4 w-[45px] h-[45px]  "></img>
+        <img
+          src={img8}
+          className="md:w-[39px] md:h-[39px]   mr-4 w-[45px] h-[45px]  "
+        ></img>
         <span className=" md:text-[14px]   text-very-dark-grey-blue text-[15px] font-extrabold	 custom-font-style  hover:text-blue-1 cursor-pointer">
           Anna Kim{" "}
         </span>{" "}
