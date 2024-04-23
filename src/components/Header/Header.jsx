e088efa20d824ce3a62206b7946cfaa07701e33d
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-23 11:33:38 +0400

    new commit!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,14 @@ function Header() {
   return (
     <section className="flex items-center justify-between">
       <div className="flex justify-center items-center gap-[11px] ">
-        <h2 className="  text-very-dark-grey-blue  font-extrabold text-3xl">
+        <h2 className=" md:text-[20px]    text-very-dark-grey-blue  font-extrabold text-3xl">
           Notifications
         </h2>
-        <div className=" text-white  font-bold text-base flex justify-center items-center w-[32px] h-[26px] rounded-[6px] bg-blue-1 ">
+        <div className=" md:text-[16px]  md:w-[32px]  md:h-[25px]  text-white  font-bold text-base flex justify-center items-center w-[32px] h-[26px] rounded-[6px] bg-blue-1 ">
           3
         </div>
       </div>
-      <h3 className="     text-dark-grey-blue text-right  font-medium text-base hover:text-blue-1 cursor-pointer	 ">
+      <h3 className="  md:text-[14px]     text-dark-grey-blue text-right  font-medium text-base hover:text-blue-1 cursor-pointer	 ">
         Mark all as read
       </h3>
     </section>
